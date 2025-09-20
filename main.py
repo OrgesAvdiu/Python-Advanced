540246f269864fbde303a2417d6dc3c78ae2df89
OrgesAvdiu <orgesiav@gmail.com>
2025-09-20 14:53:08 +0200

    modules,libraries, packages

---
+++ main.py
@@ -0,0 +1,9 @@
+# First way
+import my_math
+result = my_math.square(5)
+print(result)
+
+#Second way
+from my_math import square
+result = square(7)
+print(result)
---
+++ main4.py
@@ -0,0 +1,7 @@
+from simple_math.math_utils import add, sub
+
+sum = add(8,5)
+difference = sub(8,5)
+
+print(sum)
+print(difference)
