a03264c207ac043c77ed3e5fb0f4d198e546f023
Orges <orgesiav@gmail.com>
2025-10-28 20:45:57 +0100

    variables

---
+++ Lesson1/variables.py
@@ -0,0 +1,21 @@
+temperature = 98.6
+name = "John"
+
+print(type(temperature))
+print(type(name))
+
+age = 30
+print("Age is:", age)
+
+x = 5
+y = 3
+result = x + y
+print(result)
+
+age = age + 1
+print("Age now is:", age)
+
+first_name = "John"
+last_name = "Doe"
+full_name = first_name + " " + last_name
+print(full_name)
